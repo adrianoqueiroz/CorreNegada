363f239026df15859b8bd87633ae9949c8fb2a2c
marcusteles <marcus.tls@gmail.com>
2013-11-30 11:19:52 -0300

    Adicionando integração com o gps: pegando latitude e longitude

--- src/com/android/correnegada/ListarAtividadesActivity.java
+++ src/com/android/correnegada/ListarAtividadesActivity.java
@@ -5,6 +5,10 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -14,8 +18,11 @@ import android.widget.TabHost;
 import android.widget.Toast;
 import android.widget.TabHost.TabSpec;
 
-public class ListarAtividadesActivity extends Activity {
+public class ListarAtividadesActivity extends Activity implements LocationListener{
 
+	private double la;
+	private double lo;
+	
 	TabHost abas;
 	private static final String CAMPO_OBRIGATORIO = "Campo obrigatório";
 	
@@ -49,13 +56,15 @@ public class ListarAtividadesActivity extends Activity {
 		stb.append("Create table if not exists atividades (");
 		stb.append("_id integer primary key, ");
 		stb.append("tipoAtividade varchar(50), ");
-		stb.append("caloriasPerdidas varchar(8));");		
+		stb.append("caloriasPerdidas varchar(8), ");
+		stb.append("la double(10,9), ");
+		stb.append("lo double(10,9));");
 		db.execSQL(stb.toString());
 		
 		Cursor cursor = db.rawQuery("select * from atividades", null);
-		String[] from = {"tipoAtividade", "caloriasPerdidas"};
+		String[] from = {"tipoAtividade", "caloriasPerdidas", "la", "lo"};
 		
-		int[] to = {R.id.txvAtividade, R.id.txvCaloria}; 
+		int[] to = {R.id.txvAtividade, R.id.txvCaloria, R.id.txvLat, R.id.txvLon}; 
 		SimpleCursorAdapter adp = new SimpleCursorAdapter(getBaseContext(), R.layout.activity_listar_modelo, cursor, from , to);
 		
 		ListView ltwDados = (ListView)findViewById(R.id.atividades);
@@ -91,12 +100,22 @@ public class ListarAtividadesActivity extends Activity {
 			}else{
 			
 				try{
+					LocationManager localizacaoDispositivo = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
+					Criteria criteria = new Criteria();
+					
+					String provider = localizacaoDispositivo.NETWORK_PROVIDER;										
+					Location location = localizacaoDispositivo.getLastKnownLocation(provider);
+					
+					la = location.getLatitude();
+					lo = location.getLongitude();
 					
 					SQLiteDatabase db = openOrCreateDatabase("atividades.db", Context.MODE_PRIVATE, null);
 					
 					ContentValues ctv = new ContentValues();
 					ctv.put("tipoAtividade", tipoAtividade.getText().toString());
 					ctv.put("caloriasPerdidas", caloriasPerdidas.getText().toString());
+					ctv.put("la", la);
+					ctv.put("lo", lo);
 					
 					if (db.insert("atividades", "_id", ctv) > 0){
 						abas.setCurrentTab(0);
@@ -115,6 +134,29 @@ public class ListarAtividadesActivity extends Activity {
 	public void cancelar_click(View v){
 		
 		abas.setCurrentTab(0);
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+	
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
