ebb88561895d8a8ab8c93f0ad96b99ea7345656b
adrianoqueiroz <adriano.nq@gmail.com>
2013-12-15 20:22:10 -0300

    Localização atual

--- src/com/android/correnegada/LocaisTreinoActivity.java
+++ src/com/android/correnegada/LocaisTreinoActivity.java
@@ -1,50 +1,92 @@
 package com.android.correnegada;
 
-	import android.app.Dialog;
-	import android.os.Bundle;
-	import android.support.v4.app.FragmentActivity;
+import android.app.Dialog;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
 
-	import com.google.android.gms.common.ConnectionResult;
-	import com.google.android.gms.common.GooglePlayServicesUtil;
-	import com.google.android.gms.maps.GoogleMap;
-	import com.google.android.gms.maps.SupportMapFragment;
-	import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-	public class LocaisTreinoActivity extends FragmentActivity {
+public class LocaisTreinoActivity extends FragmentActivity {
 
-		GoogleMap googleMap;
+	GoogleMap googleMap;
 
-		Marker marker = null;
+	Marker marker = null;
 
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.activity_locais_treino);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_locais_treino);
 
-			// Getting Google Play availability status
-			int status = GooglePlayServicesUtil
-					.isGooglePlayServicesAvailable(getBaseContext());
-
-			// Showing status
-			if (status != ConnectionResult.SUCCESS) { // Google Play Services are
-														// not available
-
-				int requestCode = 10;
-				Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this,
-						requestCode);
-				dialog.show();
-
-			} else { // Google Play Services are available
-
-				// Getting reference to the SupportMapFragment of activity_main.xml
-				SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager()
-						.findFragmentById(R.id.map);
-
-				// Getting GoogleMap object from the fragment
-				googleMap = fm.getMap();
-
-			}
-
-		}
+		setUpMapIfNeeded();
 
 	}
+
+	private void setUpMapIfNeeded() {
+		// TODO Auto-generated method stub
+		if (googleMap == null) {
+			googleMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			if (googleMap != null) {
+				setUpMap();
+			}
+		}
+	}
+
+	private void setUpMap() {
+		// Enable MyLocation Layer of Google Map
+		googleMap.setMyLocationEnabled(true);
+
+		// Get LocationManager object from System Service LOCATION_SERVICE
+		LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+		// Create a criteria object to retrive provider
+		Criteria criteria = new Criteria();
+
+		// Get the name of the best provider
+		String provider = locationManager.getBestProvider(criteria, true);
+
+		// Get Current Location
+		Location myLocation = locationManager.getLastKnownLocation(provider);
+
+		// set map type
+		googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+		// Get latitude of the current location
+		double latitude = myLocation.getLatitude();
+
+		// Get longitude of the current location
+		double longitude = myLocation.getLongitude();
+
+		// Create a LatLng object for the current location
+		LatLng latLng = new LatLng(latitude, longitude);
+
+		// show the current location in Google Map
+		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+		// zoom in the google map
+		googleMap.animateCamera(CameraUpdateFactory.zoomTo(19));
+		googleMap.addMarker(new MarkerOptions().position(
+				new LatLng(latitude, longitude)).title("You are here!"));
+		
+
+		
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
